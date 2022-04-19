f5d924c7349f0066b1f728303837a3bf3a305014
ben liu <liuben@cloudfine.com>
2022-04-19 22:23:23 +0800

    feat: v7

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,15 @@
 import { defineConfig } from "vite";
+import { resolve } from "path";
+
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   root: "src",
-  base: "./",
+  base: "/",
+  build: {
+    outDir: resolve(process.cwd(), "dist"),
+  },
   plugins: [react()],
   server: {
     port: 3001,
